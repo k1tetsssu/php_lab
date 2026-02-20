f1ce923620f29c32467b6c39a25a82d986d3d91e
k1tetsssu <sereja.maev05@gmail.com>
2026-02-21 00:17:04 +0200

    add lab2

---
+++ lab2/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "Hello, World with echo!";
+
+print "Hello, World with print!<br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+// конкатенации
+echo "Количество дней: " . $days . " до отпуска! <br />";
+echo "Кто не работает тот не ест, так что " . $message . "<br /><br />";
+
+// двойных кавычек
+echo "Количество дней: $days<br />";
+echo "Сообщение: $message<br />";
+
